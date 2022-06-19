c5de3c3fbdd27e6566c463518ca2f689d9576431
ogbenisamu <samijiyemi@gmail.com>
2022-06-19 16:53:28 +0100

    minor changes

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-letters = ['a', 'b', 'c']
-for index, letter in enumerate(letters):
-    print(index, letter)
+l1 = ["eat", "sleep", "repeat"]
+s1 = "geek"
+
+obj1 = enumerate(l1)
+obj2 = enumerate(s1)
+
+print("Return Type:", type(obj1))
+print(list(enumerate(l1)))
+
+print(list(enumerate(s1, 2)))
+print(type(s1))
