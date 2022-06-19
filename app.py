57953c49a38c77ad45a790c5e65dc39aa40a7855
ogbenisamu <samijiyemi@gmail.com>
2022-06-19 18:55:20 +0100

    minor changes

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-products = [
-    ("mug", 19),
-    ("cup", 25),
-    ("phones", 200),
-    ("laptop", 27)
-]
+name1 = "ogbenisamu"
+name2 = "sam"
 
-x = list(filter(lambda item: item[1] >= 20, products))
-x_map = list(map(lambda item: item[1], products))
-print(x_map)
-print("*" * 100)
-filtered = [item[1] for item in products if item[1] >= 20]
-print(filtered)
-print("*" * 100)
-print(x)
+name1, name2 = name2, name1
+
+print("name1", name1)
+print("name2", name2)
