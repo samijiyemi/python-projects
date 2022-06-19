c187a31d071b2490d1d04f796bafee12287161cb
ogbenisamu <samijiyemi@gmail.com>
2022-06-19 18:11:03 +0100

    lambda

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-items = [
-    ("Product1", 10),
-    ("Product2", 1),
-    ("Product3", 9),
-    ("Product4", 8)
+products = [
+    ("mug", 19),
+    ("cup", 25),
+    ("phones", 200),
+    ("laptop", 27)
 ]
 
-# items.sort(key=lambda item: item[1])
-# print(items)
-
-# name = "ogbenisamu"
-# print(lambda name: name)
-# (lambda name: print(name))(name)
+x = list(filter(lambda item: item[1] >= 20, products))
+x_map = list(map(lambda item: item[1], products))
+print(x_map)
+print("*" * 100)
+filtered = [item[1] for item in products if item[1] >= 20]
+print(filtered)
+print("*" * 100)
+print(x)
