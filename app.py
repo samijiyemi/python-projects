8fa4768ae8737904602717c6110102ff9e61db22
ogbenisamu <samijiyemi@gmail.com>
2022-06-19 16:48:38 +0100

    minor changes

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-# Lists
 letters = ['a', 'b', 'c']
-zeros = [0] * 5
-combined = zeros + letters
-# print(combined)
-
-# characters
-chars = "Ogbenisamu"
-char_list = list(chars)
-char_list.append(letters)
-print(char_list)
-print(len(char_list))
+for index, letter in enumerate(letters):
+    print(index, letter)
