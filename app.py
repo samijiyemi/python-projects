9c42dd5ad923f0ab446725f8c37af8c4b0bb3882
ogbenisamu <samijiyemi@gmail.com>
2022-06-19 16:15:58 +0100

    add lists

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-def fizz_buzz(num):
-    if (num % 3 == 0) and (num % 5 == 0):
-        return "fizzbuzz"
-    if num % 3 == 0:
-        return "fizz"
-    if num % 5 == 0:
-        return "Buzz"
-    return num
-
-
-print(fizz_buzz(15))
+# Lists
+letters = ['a', 'b', 'c']
+zeros = [0] * 5
+combined = zeros + letters
+print(combined)
