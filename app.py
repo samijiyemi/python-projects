bf2cdb50f3f4cebd33155508ad3583dd6475e610
ogbenisamu <samijiyemi@gmail.com>
2022-06-19 19:28:35 +0100

    dict

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-point = {"x": 1, "y": 2}
-print(point)
-points = dict(x=1, y=2)
-print(points)
+point = dict(x=1, y=2)
+# print(point.get('a', 'Not Found!'))
+point['x'] = 10
+point['y'] = 20
+for key, value in point.items():
+    print(key, value)
