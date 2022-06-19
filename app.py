c9e7452c92065798d8e34ddac3886a09cadd7883
ogbenisamu <samijiyemi@gmail.com>
2022-06-19 16:20:47 +0100

    minor commit

--- app.py
+++ app.py
@@ -2,4 +2,11 @@
 letters = ['a', 'b', 'c']
 zeros = [0] * 5
 combined = zeros + letters
-print(combined)
+# print(combined)
+
+# characters
+chars = "Ogbenisamu"
+char_list = list(chars)
+char_list.append(letters)
+print(char_list)
+print(len(char_list))
