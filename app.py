edcc51f3d4e6ddeff4a10d171490bbee4138471b
ogbenisamu <samijiyemi@gmail.com>
2022-06-20 13:22:53 +0100

    minor chnages

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+from timeit import timeit
+
+code1 = """
+
 def calculate_xfactor(age):
-    if age <= 0:
-        raise ValueError("Age can't be zero or less")
-    return 10 / age
+     if age <= 0:
+          raise ValueError("Age can't be zero or less")
+     return 10 / age
+
+try:
+    print(calculate_xfactor(-1))
+except ValueError as error:
+    pass
+
+"""
+
+print("First Code=", timeit(code1, number=100))
