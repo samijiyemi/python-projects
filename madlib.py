6070c2270e69b875f5c7f7fea658e5ac65ce8ca2
ogbenisamu <samijiyemi@gmail.com>
2022-06-20 15:10:28 +0100

    madlibs

--- madlib.py
+++ madlib.py
@@ -1,9 +1,21 @@
-# Madlibs
 # string concatenation
 
-programming_language = ""
+programming_language = "python"
 
-# output
+# 3 ways on how to output strings
 print("Am currently learning " + programming_language)
 print("Am currently learning {}".format(programming_language))
 print(f"Am currently learning {programming_language}")
+
+# madlibs game
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+# using the third string concatenation method f string
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+# output madlib
+print(madlib)
